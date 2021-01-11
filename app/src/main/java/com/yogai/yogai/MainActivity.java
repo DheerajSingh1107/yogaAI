d6ac64abd4d611f7b06ed95ce42bed2270f1c3fa
Dheerajsingh1107 <Dheerajsingh1107@gmail.com>
2021-01-11 15:51:05 +0530

    Firebase Ui Success

--- app/src/main/java/com/yogai/yogai/MainActivity.java
+++ app/src/main/java/com/yogai/yogai/MainActivity.java
@@ -1,14 +1,74 @@
 package com.yogai.yogai;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.icu.text.UnicodeSetSpanner;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int AUTH_REQUEST_CODE = 7117 ;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener listener;
+    private List<AuthUI.IdpConfig> providers;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        providers = Arrays.asList(
+
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.AnonymousBuilder().build()
+
+        );
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                AUTH_REQUEST_CODE);
+
+    }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode ==AUTH_REQUEST_CODE)
+        {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK)
+            {
+
+                FirebaseUser user =FirebaseAuth.getInstance().getCurrentUser();
+
+                Toast.makeText(this,""+user.getEmail(),Toast.LENGTH_SHORT).show();
+
+            }
+            else
+            {
+                Toast.makeText(this,""+response.getError(),Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
