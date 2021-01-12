c09a6d681cf4dd534305f3ef18cd0e396bf5dabb
Dheerajsingh1107 <Dheerajsingh1107@gmail.com>
2021-01-12 09:32:26 +0530

    Logout successfully

--- app/src/main/java/com/yogai/yogai/MainActivity.java
+++ app/src/main/java/com/yogai/yogai/MainActivity.java
@@ -7,10 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.icu.text.UnicodeSetSpanner;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -18,12 +22,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import io.paperdb.Paper;
+
 public class MainActivity extends AppCompatActivity {
 
     private static int AUTH_REQUEST_CODE = 7117 ;
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener listener;
     private List<AuthUI.IdpConfig> providers;
+    private Button logoutButton;
 
 
 
@@ -31,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Paper.init(this);
+        logoutButton = findViewById(R.id.logout_btn);
         providers = Arrays.asList(
 
                 new AuthUI.IdpConfig.PhoneBuilder().build(),
@@ -39,16 +48,34 @@ public class MainActivity extends AppCompatActivity {
                 new AuthUI.IdpConfig.AnonymousBuilder().build()
 
         );
+        if (Paper.book().read("saved")==null) {
+            showSignInOptions();
+        }
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuthUI.getInstance()
+                        .signOut(MainActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(MainActivity.this,"Logout successfully",Toast.LENGTH_SHORT).show();
+                                showSignInOptions();
+                                Paper.book().destroy();
+                            }
+                        });
+            }
+        });
+
+    }
+
+    private void showSignInOptions() {
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setAvailableProviders(providers)
                         .build(),
                 AUTH_REQUEST_CODE);
-
     }
-
-
 
 
     @Override
@@ -63,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
                 FirebaseUser user =FirebaseAuth.getInstance().getCurrentUser();
 
                 Toast.makeText(this,""+user.getEmail(),Toast.LENGTH_SHORT).show();
+                Paper.book().write("saved","yes");
 
             }
             else
